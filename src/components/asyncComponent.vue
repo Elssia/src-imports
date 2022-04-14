3e36bcf4336537239262a9c2d8876228f2226533
Elssia <elssia@163.com>
2022-04-14 18:07:34 +0800

    1 file modified

--- src/components/asyncComponent.vue
+++ src/components/asyncComponent.vue
@@ -1,6 +1,7 @@
 <template src="../html/index.html">
 </template>
 <script setup>
+import {defineAsyncComponent,computed} from 'vue'
 const errLoadComponent = defineAsyncComponent(() => import('./errLoadComponent.vue'));
 
 </script>
