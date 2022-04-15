9ebf178bc6f5959cba0271822726535e45914f13
Elssia <elssia@163.com>
2022-04-15 09:19:47 +0800

    2 files modified

--- src/components/asyncComponent.vue
+++ src/components/asyncComponent.vue
@@ -1,7 +1,10 @@
 <template src="../html/index.html">
 </template>
 <script setup>
-import {defineAsyncComponent,computed} from 'vue'
+import {defineAsyncComponent,computed,ref} from 'vue'
 const errLoadComponent = defineAsyncComponent(() => import('./errLoadComponent.vue'));
-
+const test = ()=>{
+  console.log("6666666")
+}
+let value = ref("你好")
 </script>
